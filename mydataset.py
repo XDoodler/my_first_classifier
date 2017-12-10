3005e9c2d55cae444b0644e89e98d4dadba32849
Soham Chakrabarti <soham17041998@gmail.com>
2017-12-10 14:30:42 +0530

    updated file

--- mydataset.py
+++ mydataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 from sklearn.datasets.base import Bunch
+import matplotlib.pyplot as plt
 from sklearn import tree
 def load_my_fancy_dataset():
     with open('Jobs.csv') as csv_file:
@@ -8,13 +9,27 @@ def load_my_fancy_dataset():
         temp = next(data_file)
         n_samples = int(temp[0]) #14
         n_features = int(temp[1]) #2
+        
         features = np.empty((n_samples,n_features), dtype=np.float32)
         labels = np.empty((n_samples), dtype=np.int)
-
+        male_c=1;
+         
         for i, sample in enumerate(data_file):
             features[i] = np.asarray(sample[:-1], dtype=np.float32)
+            if(male_c%2==0):
+                 plt.plot([sample[0:1]],[sample[1:2]],'bo')
+                 male_c=male_c+1
+            else:
+                 plt.plot([sample[0:1]],[sample[1:2]],'ro')
+                 male_c=male_c+1
             labels[i] = np.asarray(sample[-1], dtype=np.int)
 
-    return Bunch(features=features, labels=labels)
+        
+           
+            
+    return Bunch(features=features, labels=labels, plot=plt)
+    
+
+
 
 
